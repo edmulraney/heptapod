d92a8c655fb65394495942aa670c9cb07a87b95b
Edward Mulraney <edmulraney@gmail.com>
2016-11-27 15:01:42 +0000

    Initial POC

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {map} from "ramda"
+import {start} from "./h/index"
+
+
+const renderer = view => ReactDOM.render(view, document.getElementById("app"))
+function UserView(props) {
+  return (
+    <div>
+      <div>Message: {props.message}</div>
+      <div>{props.users[0]}</div>
+    </div>
+  )
+}
+var app = start(renderer)
+app.subscribe(console.log)
+app.present({type: "SET_MESSAGE", payload: "yo"})
+app.present({type: "FETCH_USERS"})
--- src/h/index.js
+++ src/h/index.js
@@ -1,9 +1,12 @@
+import React from "react"
+import {filter} from "ramda"
+
 const model = {
   present: step,
   subscribers: [],
   actions: {},
   mutations: {
-    message: (proposal, model) => model.data.message = proposal.payload,
+    SET_MESSAGE: (proposal, model) => model.data.message = proposal.payload,
     FETCH_USERS: (proposal, model) => model.data.usersRequested = true,
     FETCH_USERS_SUCCESS: (proposal, model) => {
       model.data.users = proposal.payload
@@ -27,30 +30,36 @@ const model = {
 
 function step(proposal) {
   model.mutations[proposal.type] && model.mutations[proposal.type](proposal, model)
-  // model.render("hello")
+  model.render(UserView(state(model)))
   model.subscribers.forEach(subscriber => subscriber(model.data))
   nap(state(model))
 }
 
-// const api = {fetch: () => Promise.resolve(["Bob", "Alice", "Joe"])}
+const api = {fetch: () => Promise.resolve(["Bob", "Alice", "Joe"])}
 // const api = {fetch: () => Promise.reject("Not authenticated")}
 
 function state(model) {
   return {
+    // from UserViewSelector
     shouldFetchUsers: model.data.usersRequested,
+    users: model.data.users || ["loading"],
+    message: model.data.message,
   }
 }
 
 function nap(state) {
   if (state.shouldFetchUsers) {
     return api.fetch()
-      .then(users => app.present({type: "FETCH_USERS_SUCCESS", payload: users}))
-      .catch(error => app.present({type: "FETCH_USERS_FAILED", payload: error}))
+      .then(users => setTimeout(function() {
+        model.present({type: "FETCH_USERS_SUCCESS", payload: users})
+      }, 1000))
+      .catch(error => model.present({type: "FETCH_USERS_FAILED", payload: error}))
   }
 }
 
 // export function start(render) {
-function start() {
+export function start(renderer) {
+  model.render = renderer
   return {
     present: model.present,
     subscribe: model.subscribe,
@@ -58,14 +67,25 @@ function start() {
   }
 }
 
+function UserView(props) {
+  return (
+    <div>
+      <div>Message: {props.message}</div>
+      <div>{props.users[0]}</div>
+    </div>
+  )
+}
+
 // const log = model.subscribe(console.log)
 // const render = model.subscribe(() => console.log("RENDERING..")) ??
 // const syncSendMsgAction = msg => model.present({type: "message", payload: msg})
 
-////////////////////
+///////////////////
+/*
 const renderer = console.log
 
 const app = start(renderer)
 app.subscribe(renderer)
 app.present({type: "message", payload: "yo"})
 app.present({type: "FETCH_USERS"})
+*/
